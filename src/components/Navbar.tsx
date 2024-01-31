b2cd1a73dac1a7ca38defdb4a5e10d7fbeae8f1b
Stephanie Opala <rispahopala@gmail.com>
2024-01-31 16:52:40 +0300

    project initialization

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <p>Digital Journal</p>
+      <div>
+        <button>Sign In</button>
+        <button>Sign Up</button>
+        <button>Log Out</button>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
