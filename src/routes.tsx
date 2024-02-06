ae60deb61ad3a08a5f91f827776e04640a88b744
Stephanie Opala <rispahopala@gmail.com>
2024-02-06 12:02:12 +0300

    feat(routes): set up react suspense and lazy

---
+++ src/routes.tsx
@@ -0,0 +1,39 @@
+/* eslint-disable react-refresh/only-export-components */
+/* eslint-disable react/display-name */
+import { Suspense, lazy } from "react";
+import type { RouteObject } from "react-router-dom";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const Loadable = (Component: any) => (props: JSX.IntrinsicAttributes) =>
+  (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Component {...props} />
+    </Suspense>
+  );
+
+// UNAUTHENTICATED
+const Home = Loadable(lazy(() => import("./pages/Home")));
+const Login = Loadable(lazy(() => import("./pages/Login")));
+const Register = Loadable(lazy(() => import("./pages/Register")));
+
+const routes: RouteObject[] = [
+  //unauthenticated
+  {
+    path: '/',
+    element: <Home />
+  },
+
+  //TODO: Add Guest Guard
+  {
+    path: 'login',
+    element: <Login />
+  },
+  {
+    path: 'register',
+    element: <Register />
+  }
+
+  //authenticated TODO: Add AuthGuard
+]
+
+export default routes;
