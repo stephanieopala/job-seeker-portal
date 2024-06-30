8cdd5ff4a4cc1e103a38d7d8892ee846a8e1fdce
Stephanie Opala <rispahopala@gmail.com>
2024-06-30 12:42:59 +0300

    fix jobs page

--- src/pages/Jobs.tsx
+++ src/pages/Jobs.tsx
@@ -1,13 +1,21 @@
 import Footer from "@/components/navigation/Footer"
 import Navbar from "@/components/navigation/Navbar"
+// import {
+//   Table,
+//   TableBody,
+//   TableCell,
+//   TableHead,
+//   TableHeader,
+//   TableRow
+// } from "@/components/ui/table"
 
 const Jobs = () => {
   return (
     <div className="flex flex-col h-screen justify-between">
       <Navbar />
-      <div>
+        <div>
 
-      </div>
+        </div>
       <Footer />
     </div>
   )
